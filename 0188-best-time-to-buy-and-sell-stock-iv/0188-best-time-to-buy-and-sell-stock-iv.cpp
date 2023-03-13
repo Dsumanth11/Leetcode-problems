00db5651b833ff7791162c489b80fe7e0bb768f5
DonthaSumanth <81733784+Dsumanth11@users.noreply.github.com>
2023-03-13 11:31:26 +0530

    Time: 28 ms (37.57%), Space: 16.4 MB (11.40%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int maxTrans;
+    int findAns(int indx,vector<int> &p,int buyed,int trnx,vector<vector<vector<int>>> &dp)
+    {
+        if(trnx>=maxTrans)
+        {
+            return 0;
+        }
+        int ans=0;
+        if(indx==p.size()-1)
+        {
+            if(buyed==1)
+            {
+                return p[indx];
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        if(dp[indx][buyed][trnx]!=-1)
+        {
+            return dp[indx][buyed][trnx];
+        }
+        if(buyed==1)
+        {
+            //proceed
+            ans=max(ans,findAns(indx+1,p,buyed,trnx,dp));
+            //sell 
+            ans=max(ans,p[indx]+findAns(indx+1,p,0,trnx+1,dp));
+        }
+        else
+        {
+            //procced without buying
+            ans=max(ans,findAns(indx+1,p,buyed,trnx,dp));
+            //proceed with buying
+            ans=max(ans,findAns(indx+1,p,1,trnx,dp)-p[indx]);
+        }
+        return dp[indx][buyed][trnx]=ans;
+    }
+    int maxProfit(int k, vector<int>& prices) {
+        maxTrans=k;
+        vector<vector<vector<int>>> dp(prices.size()+3,vector<vector<int>>(3,vector<int>(k+2,-1)));
+        return findAns(0,prices,0,0,dp);
+    }
+};
